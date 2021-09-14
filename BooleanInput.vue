ccedd98af30368f0687f46ecff57e02520baaee8
nilshellerhoff <nilshellerhoff@users.noreply.github.com>
2021-09-14 08:00:53 +0200

    various updates

--- BooleanInput.vue
+++ BooleanInput.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <v-row no-gutters style="position: relative">
+    <v-row no-gutters style="position: relative" class="pa-0 ma-0">
+      <!-- remove (minus) button -->
+      <v-col cols="1" v-if="level > 0" :offset="level - 1" class="pt-2">
+        <v-btn v-if="removable" plain small @click="$emit('removed')">
+          <v-icon color="red">mdi-minus</v-icon>
+        </v-btn>
+      </v-col>
+
       <!-- show the operator (every row) -->
       <v-col cols="2">
         <v-select
@@ -12,47 +19,21 @@
           @change="fixChildren(condition)"
         ></v-select>
       </v-col>
-      <!-- show subtree if there are two arguments -->
-      <template v-if="condition.arguments && condition.arguments.length == 2">
-        <!-- for each argument recursively call BooleanInput -->
-        <v-row
-          v-for="argument in condition.arguments"
-          :key="argument.id"
-          no-gutters
-          style="position: relative"
-        >
-          <div
-            style="
-              position: absolute;
-              left: 8px;
-              top: 18px;
-              width: calc(5%);
-              height: 2px;
-              background-color: #888;
-            "
-          ></div>
-          <div
-            style="
-              position: absolute;
-              left: 8px;
-              top: 10px;
-              width: 2px;
-              height: 10px;
-              background-color: #888;
-            "
-          ></div>
-          <v-col offset="1">
-            <BooleanInput
-              :condition="argument"
-              :entities="entities"
-            ></BooleanInput>
-          </v-col>
-        </v-row>
+
+      <!-- add condition button -->
+      <template v-if="condition.arguments.length > 0">
+        <v-col cols="2">
+          <v-btn plain @click="addChild(condition)">
+            <v-icon color="green">mdi-plus</v-icon>
+            add condition
+          </v-btn>
+        </v-col>
       </template>
-      <!-- otherwise show the entity/item selector besides operator -->
+
+      <!-- show the entity/item selector besides operator -->
       <!-- entity selector -->
-      <template v-else>
-        <v-col cols="4">
+      <template v-if="condition.arguments.length == 0">
+        <v-col cols="3">
           <v-autocomplete
             :items="entities"
             v-model="condition.entity"
@@ -70,7 +51,7 @@
           ></v-select>
         </v-col>
         <!-- value input -->
-        <v-col cols="4">
+        <v-col cols="3">
           <!-- item selector -->
           <v-autocomplete
             v-if="currEntity.type == 'item'"
@@ -90,6 +71,44 @@
         </v-col>
       </template>
     </v-row>
+
+    <!-- show subtree if there are two arguments -->
+    <template v-if="condition.arguments && condition.arguments.length > 0">
+      <!-- for each argument recursively call BooleanInput -->
+      <div
+        v-for="argument in condition.arguments"
+        :key="argument.id"
+        style="position: relative"
+      >
+        <!-- <div
+          style="
+            position: absolute;
+            left: 8px;
+            top: 18px;
+            width: calc(5%);
+            height: 2px;
+            background-color: #888;
+          "
+        ></div>
+        <div
+          style="
+            position: absolute;
+            left: 8px;
+            top: 10px;
+            width: 2px;
+            height: 10px;
+            background-color: #888;
+          "
+        ></div> -->
+
+        <BooleanInput
+          :condition="argument"
+          :entities="entities"
+          :level="level + 1"
+          @removed="removeChild(argument.id)"
+        ></BooleanInput>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -150,6 +169,14 @@ export default {
         };
       },
     },
+
+    // level parameter is optional and should not be set manually. It will be set, when called recursively
+    //
+    // level 0 - ...
+    level: {
+      type: Number,
+      default: 0,
+    },
   },
   data: () => ({
     // selectable operators
@@ -188,6 +215,16 @@ export default {
       });
       return currEntity;
     },
+    // is the current condition removable?
+    removable() {
+      if (
+        this.$parent.condition &&
+        this.$parent.condition.arguments.length > 2
+      ) {
+        return true;
+      }
+      return false;
+    },
   },
   methods: {
     getDefaultCondition(idoffset = 0) {
@@ -206,11 +243,21 @@ export default {
       c.entity = "";
       c.value = "";
     },
+    addChild() {
+      // add a child to the arguments
+      this.condition.arguments.push(this.getDefaultCondition())
+    },
     removeChildren(c) {
       // add one child to the current node (for NOT and '' operators)
       c.entity = this.entities[0].value;
       c.value = this.entities[0].items[0].value;
       c.arguments = [];
+    },
+    removeChild(id) {
+      // remove the child identified by id from arguments
+      this.condition.arguments = this.condition.arguments.filter(
+        (arg) => arg.id != id
+      );
     },
     fixChildren(c) {
       // check the operator and if the length of arguments is not correct, call the appropriate method
@@ -232,7 +279,8 @@ export default {
       if (condition.arguments.length == 0) {
         // we have a direct entity-value condition
         // add a direct expression (i.e. column=value or column<>value)
-        sql += condition.entity + condition.comparisonOperator + condition.value;
+        sql +=
+          condition.entity + condition.comparisonOperator + condition.value;
       } else {
         // we have a subcondition
         // recursively call this method on each of the subconditions and put parentheses around it
@@ -240,8 +288,8 @@ export default {
           sql +=
             "(" + this.getSqlExpression(column, condition.arguments[i]) + ")";
 
-          // if we are in the first operation add the operator (so it stands between the arguments)
-          if (i == 0) {
+          // if we are not in the last operation add the operator (so it stands between the arguments)
+          if (i != condition.arguments.length - 1) {
             sql += " " + condition.logicOperator + " ";
           }
         }
